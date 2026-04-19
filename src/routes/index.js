c7fa94426d539ee67820c7067f6ae707dcc26b70
riya1689 <riyaratri24@gmail.com>
2026-04-19 22:57:32 +0600

    feat: implement fetching comments for posts with user metadata

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ import healthRoutes from "./health.routes.js";
 import authRoutes from "../modules/auth/auth.routes.js";
 import userRoutes from "../modules/user/user.routes.js";
 import postRoutes from "../modules/post/post.routes.js";
+import commentRoutes from "../modules/comment/comment.routes.js";
 const router = express.Router();
 
 router.use("/health", healthRoutes);
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
+router.use("/posts", commentRoutes);
 export default router;
 
